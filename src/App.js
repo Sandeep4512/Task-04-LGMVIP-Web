b8a5bf57cd7485329aaf5aac28cb78acbe4356e9
Sandeep kumar <sandeepmaurya4512@gmail.com>
2022-07-12 16:45:40 +0530

    initai commit 12/07/2022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [result, setresult] = useState("");
+
+    const handle = (e) => {
+        
+        setresult("");
+        setresult(result.concat(e.target.value));
+        
+    }
+    const clear =() =>{
+        setresult("");
+
+    }
+    const backspace = () =>{
+        setresult(result.slice(0, - 1)); // or result.length -1
+    }
+    const calculate = () =>{
+        try{
+            setresult(eval(result).toString());
+        }catch(err){
+            setresult("Error")
+        }
+       
+        
+    }
+    return (
+        <>
+            <div className='container'>
+                <form>
+                    <input type="text" placeholder='0' value={result} />
+                </form>
+                <div className='keypad'>
+                    <button id='clear' onClick={clear}>AC </button>
+                    <button id='backspace' onClick={backspace}>C</button>
+                    <button value="%" onClick={handle}>%</button>
+                    <button value="/" onClick={handle}>&divide;</button>
+                    <button value="7" onClick={handle}>7</button>
+                    <button value="8" onClick={handle}>8</button>
+                    <button value="9" onClick={handle}>9</button>
+                    <button value="*" onClick={handle}>&times;</button>
+                    <button value="4" onClick={handle}>4</button>
+                    <button value="5" onClick={handle}>5</button>
+                    <button value="6" onClick={handle}>6</button>
+                    <button value="-" onClick={handle}>&ndash;</button>
+                    <button value="1" onClick={handle}>1</button>
+                    <button value="2" onClick={handle}>2</button>
+                    <button value="3" onClick={handle}>3</button>
+                    <button value="+" onClick={handle}>+</button>
+                    <button value="0" onClick={handle}>0</button>
+                    <button value="." onClick={handle}>.</button>
+                    <button onClick={calculate}  id="equal" >=</button>
+
+
+                </div>
+
+            </div>
+        </>
+    );
 }
 
 export default App;
